7ac27ec8b79a40851b23d1ba30d4d270ea73ebfb
Abhishek Rana <abhishek971996@gmail.com>
2022-03-22 12:50:58 +0530

    Importing data from kml file and loading it in maps

--- src/componenets/mapbox-maps.js
+++ src/componenets/mapbox-maps.js
@@ -1,20 +1,18 @@
 /// app.js
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import DeckGL from "@deck.gl/react";
-import { MapView, FirstPersonView } from "@deck.gl/core";
+import { KMLLoader } from "@loaders.gl/kml";
+import { CSVLoader } from "@loaders.gl/csv";
+import { load } from "@loaders.gl/core";
+import { PolygonLayer, GeoJsonLayer } from "@deck.gl/layers";
+import {
+  StaticMap,
+  _MapContext as MapContext,
+  NavigationControl,
+} from "react-map-gl";
+import { FlyToInterpolator } from "deck.gl";
 
-import { PolygonLayer } from "@deck.gl/layers";
-import { StaticMap } from "react-map-gl";
-
-// Viewport settings
-const INITIAL_VIEW_STATE = {
-  longitude: -122.4,
-  latitude: 37.74,
-  zoom: 11,
-  maxZoom: 20,
-  pitch: 30,
-  bearing: 0,
-};
+const KML_FILE_PATH = "./data/polygon_testing.kml";
 
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoiYWJoaXNoZWstcmFuYSIsImEiOiJjbDB0cjl5d3gwb2ZiM2puNXhwMTA0Mm96In0.OP53G4BnNBQse6OJc0oNBg";
@@ -38,14 +36,94 @@ const layers = [
 ];
 
 const MapboxMaps = ({ coordinates }) => {
+  const [layers, setLayers] = useState([
+    new PolygonLayer({
+      id: "PolygonLayer",
+      data: "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-zipcodes.json",
+      extruded: true,
+      filled: true,
+      getElevation: (d) => d.population / d.area / 10,
+      getFillColor: (d) => [d.population / d.area / 60, 140, 0],
+      getLineColor: [80, 80, 80],
+      getLineWidth: (d) => 1,
+      getPolygon: (d) => d.contour,
+      lineWidthMinPixels: 1,
+      stroked: true,
+      wireframe: true,
+      pickable: true,
+    }),
+  ]);
+
+  const [viewState, setViewState] = useState({
+    latitude: 37.7751,
+    longitude: -122.4193,
+    zoom: 11,
+    bearing: 0,
+    pitch: 0,
+  });
+
+  //   const onClickGeoJson = useCallback((event) => {
+  //     const feature = event.features[0];
+  //     const [minLng, minLat, maxLng, maxLat] = bbox(feature); // Turf.js
+  //     const viewportWebMercator = new WebMercatorViewport(viewport);
+  //     const { longitude, latitude, zoom } = viewport.fitBounds(
+  //       [
+  //         [minLng, minLat],
+  //         [maxLng, maxLat],
+  //       ],
+  //       {
+  //         padding: 20,
+  //       }
+  //     );
+  //     viewportWebMercator = {
+  //       ...viewport,
+  //       longitude,
+  //       latitude,
+  //       zoom,
+  //       transitionInterpolator: new LinearInterpolator({
+  //         around: [event.offsetCenter.x, event.offsetCenter.y],
+  //       }),
+  //       transitionDuration: 1500,
+  //     };
+  //     setViewport(viewportWebMercator);
+  //   }, []);
+
+  useEffect(() => {
+    const parseData = async () => {
+      const newLayerData = await load(KML_FILE_PATH, KMLLoader);
+      console.log(newLayerData);
+
+      const newLayer = new GeoJsonLayer({
+        id: "geojson-layer",
+        data: newLayerData,
+        filled: true,
+        pickable: true,
+        getPosition: (d) => d.geometry.coordinates,
+      });
+
+      setLayers((prevLayers) => [...prevLayers, newLayer]);
+
+      setViewState({
+        longitude: 77.09341,
+        latitude: 28.59194,
+        zoom: 16,
+        pitch: 0,
+        bearing: 0,
+        transitionDuration: 4000,
+        transitionInterpolator: new FlyToInterpolator(),
+      });
+    };
+    parseData();
+  }, []);
+
   return (
     <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
+      initialViewState={viewState}
       controller={true}
       layers={layers}
-      getTooltip={({ object }) =>
-        object && `${object.zipcode}\nPopulation: ${object.population}`
-      }
+      //   getTooltip={({ object }) =>
+      //     object && `${object.zipcode}\nPopulation: ${object.population}`
+      //   }
     >
       <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
     </DeckGL>
